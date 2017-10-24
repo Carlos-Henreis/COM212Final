7efa82c61ec2989c9584f84276fcbd0abee49e5d
Carlos-Henreis <carlos_henreis@unifei.edu.br>
2017-10-24 15:09:21 -0200

    Estrutura do nosso projeto implementada

---
+++ SISCOOG/app/Providers/AuthServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace SICOOG\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'SICOOG\Model' => 'SICOOG\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        //
+    }
+}
