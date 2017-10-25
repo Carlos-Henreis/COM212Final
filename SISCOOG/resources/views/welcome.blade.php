e518d40113f2199ca725f3539d14c288e9122c77
Jean Carlos de Oliveira <jeancarlosdeoliveira@outlook.com>
2017-10-25 13:10:16 -0200

    Main Page
    Main Page

--- SISCOOG/resources/views/welcome.blade.php
+++ SISCOOG/resources/views/welcome.blade.php
@@ -79,15 +79,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    SISCOOG
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="links" style="font-weight: bold">
+                    
+                    Sistema de Cooperação em Grupo
+                
                 </div>
             </div>
         </div>
