e60892516eb121fdff95302f65a735fa366db608
Jean Carlos de Oliveira <jeancarlosdeoliveira@outlook.com>
2017-10-25 19:30:36 -0200

    Finalizando Home
    Visual Hoje

--- SISCOOG/resources/views/home.blade.php
+++ SISCOOG/resources/views/home.blade.php
@@ -5,16 +5,23 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                <div class="panel-heading">Dashboard
+                    
 
-                    You are logged in!
+                </div>
+                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        
+                        <!-- Authentication Links -->
+                            <li><a href="#">Meus Grupos</a></li>
+                            <li><a href="#">Amigos</a></li>
+                    </ul>   
+                 </div>
+                    <div class="panel-body">
+                        
+                    Vc está Logado 🙂
+                    </div>
                 </div>
             </div>
         </div>
